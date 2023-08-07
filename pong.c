7f23cf5f486da3e520f80e5f36af0f3f808cdbb8
yyinyangg <131518880+yyinyangg@users.noreply.github.com>
2023-08-07 16:10:16 +0200

    Update pong.c

--- pong.c
+++ pong.c
@@ -1,197 +1,175 @@
-#include "pong.h"
-#include "delay.h"
+#include "src/display.h"
+
+#include "gfx.h"
+#include "glcdfont.h"
 #include <stdio.h>
-#include <string.h>
-void initialBall(){
-    ball.ballX = 240;
-    ball.ballY = 160;
-    ball.ballDirX = -15;
-    ball.ballDirY = 0;
-    ball.ballSize = 10;
+
+static int16_t cursorX, cursorY;
+static uint16_t textColor;
+static uint8_t textSize;
+static int textBackground;
+
+uint16_t color565_s(const uint8_t r, const uint8_t g, const uint8_t b) {
+    const uint8_t hiR = (r & 0xF8) >> 3;
+    const uint8_t hiG = (g & 0xFC) >> 2;
+    const uint8_t hiB = (b & 0xF8) >> 3;
+    const uint16_t result = (hiR << 11) | (hiG << 5) | hiB;
+    return result;
+    // A more compact solution is:
+    // return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
 
-void calPaddleBoundary(struct Paddle *p) {
-    p->upperBound = p->paddle_posY+p->paddle_size;
-    p->lowerBound = p->paddle_posY-p->paddle_size;
-    if(p->upperBound > 320)
-      p->paddle_posY = 320 - p->paddle_size;
-    else if(p->lowerBound < 0)
-      p->paddle_posY = 0 + p->paddle_size;
-    //printf("the %d paddleUpperbound is %d, LowerBound is %d ",p->id, p->upperBound,p->lowerBound);
-}
-
-void calBallBoundary(struct Ball *b) {
-    b->upperBound = b->ballY + b->ballSize;
-    b->lowerBound = b->ballY - b->ballSize;
-    b->leftBound = b->ballX - b->ballSize;
-    b->rightBound = b->ballX + b->ballSize;
-    checkFieldBoundary(b);
-}
-void initialPong() {
-    initialBall();
-    calBallBoundary(&ball);
-    
-    paddle1.paddle_posX = 0;
-    paddle1.paddle_posY = 160;
-    paddle1.paddle_dir = 0;
-    paddle1.paddle_size = 30;
-    paddle1.thickness = 8;
-    paddle1.id = 1;
-    paddle1.baseline = paddle1.paddle_posX + paddle1.thickness;
-    paddle1.score = 0;
-    calPaddleBoundary(&paddle1);
-    
-    paddle2.paddle_posX = 479;
-    paddle2.paddle_posY = 160;
-    paddle2.paddle_dir = 0;
-    paddle2.paddle_size = 30;
-    paddle2.thickness = 8;
-    paddle2.id = 0;
-    paddle2.baseline = paddle2.paddle_posX - paddle2.thickness;
-    paddle2.score = 0;
-    calPaddleBoundary(&paddle2);
-       
-    printPattern(BLACK, WHITE);
-}
-
-uint8_t checkCollision(struct Ball *b, struct Paddle *p){
-  
-    uint8_t rtn = 0;
-    if(b->upperBound <= p->upperBound && b->lowerBound >= p->lowerBound){
-      rtn = 1;
-      b->ballDirY = p->paddle_dir;
-      b->ballDirX = - b->ballDirX;
-    }
-  
-    return rtn;
-}
-
-void checkFieldBoundary(struct Ball *b){
-
-  if(b->upperBound >= 320){
-    b->ballY = 320 - b->ballSize;
-    b->ballDirY = - b->ballDirY;
-  }
-  else if (b->lowerBound <= 0){
-    b->ballY = 0 + b->ballSize;
-    b->ballDirY = -b->ballDirY;
-  }
-}
-
-void readJoystick() {
-    uint8_t analog11;
-    uint8_t analog12;
-    
-    uint8_t analog13;
-    uint8_t analog16;
-    uint8_t analog19;
-    uint8_t analog23;
-    
-    uint8_t analog17;
-    cppp_getAnalogValues(&analog11, &analog12, &analog13, &analog16, &analog17, &analog19, &analog23);
-    const uint32_t sleepTime = 10000; // 0,01s
-
-
-    // joystick left
-    if (analog19 >= 220) {
-        paddle1.paddle_dir = -20;
-    }
-    // joystick middle
-    else if (analog19 < 220 && analog19 >= 180) {
-        paddle1.paddle_dir = 0;
-    }
-    // joystick right
-    else if (analog19 < 180) {
-        paddle1.paddle_dir = 20;
-    }
-    
-    if (analog23 >= 220) {
-        paddle2.paddle_dir = -20;
-    }
-    // joystick middle
-    else if (analog23 < 220 && analog23 >= 180) {
-        paddle2.paddle_dir = 0;
-    }
-    // joystick right
-    else if (analog23 < 180) {
-        paddle2.paddle_dir = 20;
-    }
-    
-    cppp_microDelay(sleepTime);
-}
-
-void update(){
-    const uint32_t sleepTime = 100000; // 0,1s
-    ball.ballX = ball.ballX + ball.ballDirX;
-    ball.ballY = ball.ballY + ball.ballDirY;
-    
-    paddle1.paddle_posY = paddle1.paddle_posY + paddle1.paddle_dir;
-    //calPaddleBoundary(&paddle1);
-    paddle2.paddle_posY = paddle2.paddle_posY + paddle2.paddle_dir;
-    //calPaddleBoundary(&paddle2);
-    //cppp_microDelay(sleepTime);
-}
-
-void showField(){
-
-    cppp_fillCircle(ball.ballX, ball.ballY, ball.ballSize, YELLOW);
-    
-    cppp_fillRect(paddle1.paddle_posX, paddle1.paddle_posY - paddle1.paddle_size, paddle1.thickness, 2*paddle1.paddle_size, WHITE);
-    
-    cppp_fillRect(paddle2.paddle_posX-paddle2.thickness, paddle2.paddle_posY - paddle2.paddle_size, paddle2.thickness, 2*paddle1.paddle_size, WHITE);
-  /*  
-    char title1[10] ="ID: ";
-    char *id1,*s1;
-    sprintf(id1,"%u",paddle1.id);
-    sprintf(s1,"%u",paddle1.score);
-    strcat(title1,id1);
-    strcat(title1,s1);
-
-    char title2[10] = "ID: ";
-    char *id2,*s2;
-    sprintf(id2,"%u",paddle2.id);
-    sprintf(s2,"%u",paddle2.score);
-    strcat(title2,id2);
-    strcat(title2,s2);
-
-    char message[30]=" Score ";
-    strcat(message,title1);
-    strcat(message,title2);
-    writeText_s(message);
-  */  
-    //writeNumberOnDisplay_s(&paddle1.score);
-    //writeNumberOnDisplayRight_s(&paddle2.score);
-    
-} 
-
-void checkScore(){
-  
-    if(ball.leftBound<=paddle1.baseline){     
-      if(!checkCollision(&ball, &paddle1)){
-        paddle2.score +=1;
-        initialBall();     
-      }   
-    }
-    else if (ball.rightBound>=paddle2.baseline){
-      if(!checkCollision(&ball, &paddle2)){
-        paddle1.score +=1;
-        initialBall();
-      }   
+void printPattern_s(const uint16_t backgroundColor, const uint16_t foregroundColor) {
+    int j;
+    uint8_t blocksize = 4;
+    cppp_fillScreen(backgroundColor);
+    for (int i = 0; i < 480; i += blocksize) {
+        if (i % (blocksize * 2) == 0)
+            j = 0;
+        else
+            j = blocksize;
+        for (j; j < 320; j += blocksize * 2)
+            cppp_fillRect(i, j, blocksize, blocksize, foregroundColor);
     }
 }
 
-void startPong(){
-  const uint32_t sleepTime = 10000; // 0,1s
-  initialPong();
-  while(1){
-    readJoystick();
-    update();
-    calBallBoundary(&ball);
-    calPaddleBoundary(&paddle1);
-    calPaddleBoundary(&paddle2);
-    checkScore();
-    printPattern(BLACK, WHITE);
-    showField();
-    cppp_microDelay(sleepTime);      
-  }  
+void initCursor_s() {
+    cursorX = 0;
+    cursorY = 0;
+    textColor = WHITE;
+    textSize = 2;
+    textBackground = BLACK;
+}
+
+void setCursor_s(const int16_t x, const int16_t y) {
+    cursorX = x;
+    cursorY = y;
+}
+
+void setTextColor_s(const uint16_t c) {
+    // For 'transparent' background, we'll set the bg to the same as the rest of the display
+    textColor = c;
+}
+
+void setTextSize_s(const uint8_t s) {
+    textSize = (s > 0) ? s : 1;
+}
+
+void setBackgroundColor_s(const int bg) {
+    textBackground = bg;
+}
+
+void drawChar_s(int x, int y, char c, int color, int bg, char size) {
+    if ((x >= 480) ||               // Clip right
+        (y >= 320) ||               // Clip top
+        ((x + 6 * size - 1) < 0) || // Clip left
+        ((y + 8 * size - 1) < 0)) { // Clip bottom
+        return;
+    }
+
+    char i, j;
+    for (i = 0; i < 6; i++) { // Draw in x-direction
+        char line;            // Vertical line
+        if (i < 5)
+            line = font[(c * 5) + i]; // Save the i.x-line from (i,j) to (i,j+7) in the char line
+        else
+            line = 0x0;                       // Padding line toward next character
+        for (j = 0; j < 8; j++, line >>= 1) { // Draw in y-direction
+            if (line & 0x1) {
+                if (size == 1)
+                    cppp_drawPixel(x + i, y - j, color);
+                else
+                    cppp_fillRect(x + (i * size), y - ((j + 1) * size) + 1, size, size, color); //keep in mind cppp_fillRect wants the lower left corner
+            }
+            else if (bg != color) {
+                if (size == 1)
+                    cppp_drawPixel(x + i, y - j, bg);
+                else
+                    cppp_fillRect(x + (i * size), y - ((j + 1) * size) + 1, size, size, bg);
+            }
+        }
+    }
+}
+
+void writeAuto_s(const char c) {
+    if (c == '\n') {
+        cursorY -= textSize * 8;
+        cursorX = 0;
+    }
+    else if (c == '\r') {
+        // nop
+    }
+    else {
+        if (((cursorX + textSize * 6) > 480)) { // Heading off edge
+            cursorX = 0;                        // Reset x to zero
+            cursorY -= textSize * 8;            // Advance y one line
+        }
+        if (((cursorY - textSize * 6) < 0)) { // Heading off edge
+            cursorY = 319;                    // Advance y one line
+            cursorX = 0;                      // Reset x to zero
+        }
+        cursorX = textSize * 6;
+        drawChar_s(cursorX, cursorY, c, textColor, textBackground, textSize);
+        //cursorX += textSize * 6;
+    }
+}
+
+void writeText_s(const char* text) {
+    cursorY = 0;
+    cursorX = 0;
+    for (int i = 0; text[i] != 0; i++) {
+        writeAuto_s(text[i]);
+    }
+}
+
+void writeTextln_s(const char* text) {
+    cursorY = textSize * 8;
+    cursorX = 0;
+    for (int i = 0; text[i] != 0; i++) {
+        writeAuto_s(text[i]);
+    }
+    
+}
+
+void writeNumberOnDisplay_s(const uint8_t* value) {
+    char buffer[4];
+    sprintf(buffer, "%u", *value);
+    writeText_s(buffer);
+}
+
+void writeNumberOnDisplayRight_s(const uint8_t* value) {
+    int x = 100;
+      char buffer[4];
+      char* bufferStart = buffer;
+      while(x > 1 && *value < x) {
+          *bufferStart = ' ';
+          bufferStart++;
+          x /= 10;
+      }
+      sprintf(bufferStart, "%u", *value);
+      writeTextln_s(buffer);
+}
+
+void write16BitNumberOnDisplay_s(const uint16_t* value, uint8_t mode) {
+    int x = 10000;
+    if (mode == 1) {
+        uint8_t numberOfDigits = 6;
+        while(x > 1 && *value < x) {
+            numberOfDigits--;
+          x /= 10;
+        }
+        char buffer[numberOfDigits];
+        sprintf(buffer, "%u", *value);
+        writeText_s(buffer);
+    } else {
+        char buffer[6];
+        char* bufferStart = buffer;
+        while(x > 1 && *value < x) {
+            *bufferStart = ' ';
+            bufferStart++;
+            x /= 10;
+        }
+        sprintf(bufferStart, "%u", *value);
+        writeText_s(buffer); 
+    }
 }
